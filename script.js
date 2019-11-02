01e0f3467138e133d2c4f8ccae1b7d903ca1b1f6
Elena Capienko <elena.capienko@gmail.com>
2019-11-02 15:46:12 -0400

    init: add sample for style classes (to be deleted)

---
+++ script.js
@@ -0,0 +1,52 @@
+// let textMe = document.createElement('textarea');
+// let body = document.querySelector('body');
+// body.prepend(textMe);
+
+const KeyBoard = {
+
+    elements: {
+        main: null,
+        itemsContainer: null,
+        items: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    start() {
+
+    },
+
+    _addItems() {
+
+    },
+
+    _triggerEvents(handlerName) {
+
+    },
+
+    _toggleCapsLock() {
+
+    },
+
+    writing(initialValue, oninput) {
+
+    }
+}    
+    
+window.onload(function() {
+    KeyBoard.start();
+});
+    
+// document.createElement('div');
+// let texting = document.createElement('textarea');
+// let body = document.querySelector('body');
+// body.prepend(keyboard);
+// body.prepend(texting);
+    
